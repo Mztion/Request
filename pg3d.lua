ea43141882dbd5c9a9f4629469bb1bd5476b4be2
Mztion <139071362+Mztion@users.noreply.github.com>
2023-08-21 10:32:55 -0700

    Update pg3d.lua

--- pg3d.lua
+++ pg3d.lua
@@ -1,6 +1,8 @@
 function Choices ()
     
-    local Choice_1 = gg.choice({"[ Start Script ]", "[ Exit ]"})
+    local GamePackageName = gg.getTargetPackage()
+
+    local Choice_1 = gg.choice({"[ Start Script ]", "[ Exit ]"},nil, "pg3d.lua - " .. GamePackageName)
     if Choice_1 == 1 then
         
         local Choice_2 = gg.choice({"Player", "Weapons"})
