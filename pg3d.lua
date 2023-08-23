2e7d3a276f9003c2aa4f47dab9ffd01610399e27
Mztion <139071362+Mztion@users.noreply.github.com>
2023-08-23 09:57:13 -0700

    Update pg3d.lua

--- pg3d.lua
+++ pg3d.lua
@@ -1,29 +1,21 @@
-function Choices ()
+local GamePackageName = gg.getTargetPackage()
+
+function Main ()
     
-    local GamePackageName = gg.getTargetPackage()
+    local Choice_Home = gg.choice({"[ Start Script ]", "[ Exit ]"},nil, GamePackageName)
+    if Choice_Home == 1 then
 
-    local Choice_1 = gg.choice({"[ Start Script ]", "[ Exit ]"},nil, "pg3d.lua | " .. GamePackageName)
-    if Choice_1 == 1 then
-        
-        local Choice_2 = gg.choice({"[ Player ]", "[ Weapons ]"},nil, "Home")
-        if Choice_2 == 1 then
+        if GamePackageName ~= "com.pixel.gun3d" then
             
-            local Choice_3 = gg.choice({"[ Level ]"},nil, "Home > Player")
-            if Choice_3 == 1 then
-                
-                gg.setRanges(gg.REGION_CODE_APP)
-                gg.searchNumber("h 00 00 50 E3 04 00 00 1A", gg.TYPE_BYTE)
-                gg.getResults(gg.getResultsCount())
-                gg.editAll("h 85 0F 07 E3 1E FF 2F E1", gg.TYPE_BYTE)
+            gg.alert("Pixel Gun 3D not selected in GameGuardian")
+            Main()
 
-            end
+        else
+
+            gg.choice({""})
 
         end
 
-    elseif Choice_1 == 2 then
-
-        os.exit()
-
     end
 
-end Choices()
+end Main()
