863601429b5799ab4f4135de28f8bb3f9ec8f087
Mztion <139071362+Mztion@users.noreply.github.com>
2023-08-22 17:52:41 -0700

    Update pg3d.lua

--- pg3d.lua
+++ pg3d.lua
@@ -12,9 +12,9 @@ function Choices ()
             if Choice_3 == 1 then
                 
                 gg.setRanges(gg.REGION_CODE_APP)
-                gg.searchNumber("h 00 00 50 E3 04 00 00 1A 54 00 9F E5 00 00 9F E7", gg.TYPE_DWORD)
+                gg.searchNumber("h 00 00 50 E3 04 00 00 1A", gg.TYPE_BYTE)
                 gg.getResults(gg.getResultsCount())
-                gg.editAll("h A0 F0 8F D2 C0 03 5F D6 F3 7B 02 A9 B4 F6 01 D0 88 BA 78 39 F3 03 00 AA 28 01 00 37 E0 D3 01 90", gg.TYPE_DWORD)
+                gg.editAll("h 85 0F 07 E3 1E FF 2F E1", gg.TYPE_BYTE)
 
             end
 
